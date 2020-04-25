cd91dfb0255204f533e2d996f50c32b86718c38d
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-25 20:43:59 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -3,5 +3,10 @@ def find_max_value(array)
   max_value = -1
   
   while count < array.length do 
-    if 
+    if max_value < array[count]
+      max_value = array[count]
+    end
+    count += 1 
+  end
+  max_value
 end
